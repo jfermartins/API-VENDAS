8f629fbb6b20bbd4cfe1cb94733f1982e547036b
Jane Fernanda Martins <jfermartins@hotmail.com>
2024-01-30 05:01:59 -0300

    Migrations products

--- src/shared/typeorm/migrations/1706599898153-CreateProducts.ts
+++ src/shared/typeorm/migrations/1706599898153-CreateProducts.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateProducts1607437608841 implements MigrationInterface {
+export class CreateProducts1706599898153 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(

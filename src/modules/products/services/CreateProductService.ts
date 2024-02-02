aa30c5c5423d6ce60778ce2f88dbbe4ae6b19845
Jane Fernanda Martins <jfermartins@hotmail.com>
2024-02-02 01:54:36 -0300

    Routes e testes

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -1,4 +1,3 @@
-//import redisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
@@ -19,15 +18,11 @@ class CreateProductService {
       throw new AppError('There is already one product with this name');
     }
 
-    //const redisCache = new RedisCache();
-
     const product = productsRepository.create({
       name,
       price,
       quantity,
     });
-
-    //await redisCache.invalidate('api-vendas-PRODUCT_LIST');
 
     await productsRepository.save(product);
 

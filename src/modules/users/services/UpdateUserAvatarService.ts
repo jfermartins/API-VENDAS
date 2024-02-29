c24a4fb0cc97a5b1ccfe87db7c47f5759fd81eb5
Jane Fernanda Martins <jfermartins@hotmail.com>
2024-02-29 03:10:36 -0300

    Create avatar

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,8 +1,10 @@
 import AppError from '@shared/errors/AppError';
+import path from 'path';
+import fs from 'fs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
-import path from 'path';
+import uploadConfig from '@config/upload';
 
 interface IRequest {
   user_id: string;
@@ -13,15 +15,26 @@ class UpdateUserAvatarService {
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
     const usersRepository = getCustomRepository(UsersRepository);
 
-    const user = await  usersRepository.findById(user_id);
+    const user = await usersRepository.findById(user_id);
 
     if (!user) {
       throw new AppError('User not found. ');
     }
 
     if (user.avatar) {
-      const userAvatarFilePath = path.join
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
     }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
   }
 }
 

1f373987832317dc3a47002655f64fc168a81f08
Jane Fernanda Martins <jfermartins@hotmail.com>
2024-04-05 05:50:05 -0300

    Implementação da classe RedisCache

--- src/shared/cache/RedisCache.ts
+++ src/shared/cache/RedisCache.ts
@@ -1,4 +1,4 @@
-import Redis, { RedisClient } from 'redis';
+import Redis, { Redis as RedisClient } from 'ioredis';
 import cacheConfig from '@config/cache';
 
 export default class RedisCache {
@@ -7,4 +7,12 @@ export default class RedisCache {
   constructor() {
     this.client = new Redis(cacheConfig.config.redis);
   }
+
+  public async save(key: string, value: any): Promise<void> {
+    await this.client.set(key, JSON.stringify(value));
+  }
+
+  //public async recover<T>>(key: string): Promise<T | null> {}
+
+  //public async invalidate(key: string): Promise<void> {}
 }

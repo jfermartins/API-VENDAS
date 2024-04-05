114585e714e8c9105bf9d4b42ef8636968be0c40
Jane Fernanda Martins <jfermartins@hotmail.com>
2024-04-05 05:08:12 -0300

    Criação da classe RedisCache

---
+++ src/shared/cache/RedisCache.ts
@@ -0,0 +1,10 @@
+import Redis, { RedisClient } from 'redis';
+import cacheConfig from '@config/cache';
+
+export default class RedisCache {
+  private client: RedisClient;
+
+  constructor() {
+    this.client = new Redis(cacheConfig.config.redis);
+  }
+}

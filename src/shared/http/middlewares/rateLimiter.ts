e5121fdfbd6e94cb558c22b575a22c12bfa20bd6
Jane Fernanda Martins <jfermartins@hotmail.com>
2024-05-03 04:15:30 -0300

    Rate Limiter

--- src/shared/http/middlewares/rateLimiter.ts
+++ src/shared/http/middlewares/rateLimiter.ts
@@ -1,17 +1,31 @@
 import { Request, Response, NextFunction } from 'express';
-import redis from 'redis';
+import Redis from 'ioredis';
 import { RateLimiterRedis } from 'rate-limiter-flexible';
 import AppError from '@shared/errors/AppError';
 
-const redisClient = redis.CreateClient({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASS || undefined,
-});
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    const redisClient = new Redis({
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+      password: process.env.REDIS_PASS || undefined,
+    });
 
-const limiter = new RateLimiterRedis({
-  storeClient: redisClient,
-  keyPrefix: 'ratelimit',
-  points: 5,
-  duration: 1,
-});
+    const limiter = new RateLimiterRedis({
+      storeClient: redisClient,
+      keyPrefix: 'ratelimit',
+      points: 5,
+      duration: 1,
+    });
+
+    await limiter.consume(request.ip as string | number);
+
+    return next();
+  } catch (err) {
+    throw new AppError('Too many requests.', 429);
+  }
+}

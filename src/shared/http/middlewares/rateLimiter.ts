3348262839dd4d0ef4f3339b7c628ba411a4ebb9
Jane Fernanda Martins <jfermartins@hotmail.com>
2024-05-03 03:42:53 -0300

    Rate Limiter

---
+++ src/shared/http/middlewares/rateLimiter.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import redis from 'redis';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import AppError from '@shared/errors/AppError';
+
+const redisClient = redis.CreateClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASS || undefined,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'ratelimit',
+  points: 5,
+  duration: 1,
+});
